457180a4d507f8fb59e8bc336fc0a0096e31223c
Michiel Boekhoff <michielalexanderb@gmail.com>
2022-02-02 20:38:52 +0000

    Nixify all the things

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A demo for Otomi";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ terraform terraform-ls ];
+        };
+
+      });
+}
